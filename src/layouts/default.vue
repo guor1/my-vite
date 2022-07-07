d18450e01ccbf6a6b598da5d27441789e17749a1
guorui1 <guorui1@111.com.cn>
2022-07-07 17:51:45 +0800

    1

---
+++ src/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <n-layout class="min-h-screen">
+    <main class="px-4 py-10 text-center">
+      <router-view />
+      <Footer />
+      <div class="mt-5 mx-auto text-center text-sm">
+        [Default Layout]
+      </div>
+    </main>
+  </n-layout>
+</template>
